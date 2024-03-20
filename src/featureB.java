86d6c8364f74deb7034f4ee7363d8f13a0c8364a
iqbalya25 <iqbalya25@gmail.com>
2024-03-20 15:59:47 +0700

    adding feature

--- src/featureB.java
+++ src/featureB.java
@@ -1,5 +1,8 @@
 public class featureB {
     public static void main(String[] args) {
         System.out.println("new feature");
+        System.out.println("new feature");
+        System.out.println("new feature");
+        System.out.println("new feature");
     }
 }
